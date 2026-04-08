b752e54721905d5c0df73b309658cd64729a65ea
mhf <mhfcbz@163.com>
2026-04-08 23:33:08 +0800

    s:

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::error::Error;
 
 /**
 usage:
-    u ${cmd}
+	u ${cmd}
 */
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let cmd = u::Config::new(args);
-    if let Err(err) = cmd.run() {
-        let a = format!("{}", err.to_string());
-        return Err(Box::<dyn Error>::from(a));
-    }
-    Ok(())
+	let args: Vec<String> = env::args().collect();
+	let cmd = u::Config::new(args);
+	if let Err(err) = cmd.run() {
+		let a = format!("{}", err.to_string());
+		return Err(Box::<dyn Error>::from(a));
+	}
+	Ok(())
 }
